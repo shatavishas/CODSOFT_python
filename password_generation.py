96f45e2b6fb882558d64b9b653b4ce2f04da329f
shatavishas <167270103+shatavishas@users.noreply.github.com>
2024-04-22 21:36:33 -0700

    Update password_generation.py

--- password_generation.py
+++ password_generation.py
@@ -1,7 +1,6 @@
 import random
-#name=input("enter your name")
 while i<=8:
-    x=''.join(chr(random.randint(65,90)))
+    x=''.join(chr(random.randint(33,126)))
     i+=1
 print("your new password is:")
 print(x)
