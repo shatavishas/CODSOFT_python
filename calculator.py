120840a259082eee7468d4ed85ce5d18928e1d6a
shatavishas <167270103+shatavishas@users.noreply.github.com>
2024-04-22 22:09:26 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,10 +3,14 @@ y=int(input("enter the second number"))
 symbol=input("enter any of the basic symbol to perform airthmetic operation:+,-,*,/:  ")
 if (symbol=="+"):
     z=x+y
+    print("the sum of the two numbers is:")
 elif(symbol=="-"):
     z=x-y
+    print("the difference of the two numbers is:")
 elif(symbol=="*"):
     z=x*y
+    print("the product of the two numbers is:")
 elif(symbol=="/"):
     z=x/y
+    print("the division of the two numbers gives:")
 print(z)
